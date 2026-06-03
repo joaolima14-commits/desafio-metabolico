0349b52bb48f78dc0634a8f298407593ffdbe09d
joaolima14-commits <joao.lima_14@hotmail.com>
2026-06-03 14:44:16 -0300

    Limpar cache antigo do app

--- sw.js
+++ sw.js
@@ -1,6 +1,23 @@
+const CACHE_NAME = "dm7-cache-v3-anti-duplicidade";
 
-const CACHE='desafio-metabolico-v1';
-const ASSETS=['./','./index.html','./styles.css','./app.js','./pages.json','./manifest.webmanifest'];
-self.addEventListener('install',event=>{event.waitUntil(caches.open(CACHE).then(cache=>cache.addAll(ASSETS)).then(()=>self.skipWaiting()))});
-self.addEventListener('activate',event=>{event.waitUntil(self.clients.claim())});
-self.addEventListener('fetch',event=>{event.respondWith(caches.match(event.request).then(cached=>cached||fetch(event.request).then(resp=>{const copy=resp.clone();caches.open(CACHE).then(cache=>cache.put(event.request,copy));return resp}).catch(()=>cached))) });
+self.addEventListener("install", function (event) {
+  self.skipWaiting();
+});
+
+self.addEventListener("activate", function (event) {
+  event.waitUntil(
+    caches.keys().then(function (cacheNames) {
+      return Promise.all(
+        cacheNames.map(function (cacheName) {
+          return caches.delete(cacheName);
+        })
+      );
+    }).then(function () {
+      return self.clients.claim();
+    })
+  );
+});
+
+self.addEventListener("fetch", function (event) {
+  event.respondWith(fetch(event.request));
+});
